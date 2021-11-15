01c49b5998638b2779c37d58e6a2c56b704c7fcd
Lucas <lucas@maple.finance>
2021-11-14 22:43:58 -0500

    feat: update test.sh comments

--- test.sh
+++ test.sh
@@ -18,15 +18,12 @@ skip_build=$([ "$build" == "0" ] && echo "1" || echo "0")
 
 [[ $SKIP_MAINNET_CHECK || "$ETH_RPC_URL" && "$(seth chain)" == "ethlive" ]] || { echo "Please set a mainnet ETH_RPC_URL"; exit 1; }
 
-# Wednesday, October 27, 2021 12:58:18 PM UTC
+# Sunday, November 14, 2021 3:02:17 AM UTC
 export DAPP_TEST_TIMESTAMP=1636858937
 export DAPP_TEST_NUMBER=13611498
 
 export DAPP_SOLC_VERSION=0.6.12
-# export DAPP_SRC="contracts"
-# export DAPP_LINK_TEST_LIBRARIES=0
-# export DAPP_STANDARD_JSON=$config
-export DAPP_TEST_ADDRESS=0xfeb4acf3df3cdea7399794d0869ef76a6efaff52 
+export DAPP_TEST_ADDRESS=0xfeb4acf3df3cdea7399794d0869ef76a6efaff52  # Yearn governance address
 
 if [ "$skip_build" = "1" ]; then export DAPP_SKIP_BUILD=1; fi
 
